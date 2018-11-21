bdd5367e38d53c6038e9ccd5445342e7393e17d6
Andrey Chernyshev <andrey.chernyshev@simbirsoft.com>
2018-11-21 11:46:44 +0300

    remove unused

--- Podfile
+++ Podfile
@@ -3,8 +3,5 @@ inhibit_all_warnings!
 use_frameworks!
 
 target 'iOSMobileArchitecture' do
-  pod 'Alamofire'
-  pod 'RxSwift'
-  pod 'RxCocoa'
-  pod 'RealmSwift'
+  
 end
--- iOSMobileArchitecture/Classes/Presentation/SplashScreen/SplashViewController.swift
+++ iOSMobileArchitecture/Classes/Presentation/SplashScreen/SplashViewController.swift
@@ -7,37 +7,11 @@
 //
 
 import UIKit
-import RxSwift
-import RxCocoa
 
 class SplashViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        SessionService
-            .userIsAuthorized
-            .delaySubscription(2, scheduler: MainScheduler.asyncInstance)
-            .subscribe(onNext: { [weak self] isAuthorized in
-                if isAuthorized {
-                    self?.goToCitiesScreen()
-                } else {
-                    self?.goToAuthorizationScreen()
-                }
-            })
-            .disposed(by: disposeBag)
+        
     }
-    
-    private func goToAuthorizationScreen() {
-        let vc = UIStoryboard(name: "AuthorizationScreen", bundle: nil).instantiateViewController(withIdentifier: "AuthorizationViewController")
-        let nc = UINavigationController(rootViewController: vc)
-        UIApplication.shared.keyWindow?.rootViewController = nc
-    }
-    
-    private func goToCitiesScreen() {
-        let vc = UIStoryboard(name: "CitiesScreen", bundle: nil).instantiateViewController(withIdentifier: "CitiesViewController")
-        let nc = UINavigationController(rootViewController: vc)
-        UIApplication.shared.keyWindow?.rootViewController = nc
-    }
-    
-    private let disposeBag = DisposeBag()
 }
